3b6c793ae00faf9707a9f93853fc4d66d36b8a51
Chaoqun ZHANG <37767948+Warlocat@users.noreply.github.com>
2020-02-11 19:35:56 -0500

    self use

--- gto_test/gto.cpp
+++ gto_test/gto.cpp
@@ -7,6 +7,63 @@
 using namespace std;
 using namespace Eigen;
 
+double double_factorial(const int& n)
+{
+    switch (n)
+    {
+    case 0: return 1.0;
+    case 2: return 2.0;
+    case 4: return 8.0;
+    case 6: return 48.0;
+    case 8: return 384.0;
+    case 10: return 3840.0;
+    case 12: return 46080.0;
+    case 14: return 645120.0;
+    default:
+        if(n < 0)
+        {
+            cout << "ERROR: double_factorial is called for a negative number!" << endl;
+            exit(99);
+        }
+        double tmp = 645120.0;
+        for(int ii = 16; ii <= n; ii = ii + 2)
+        {
+            tmp = tmp * ii;
+        }
+        return tmp;
+    }
+}
+double factorial(const int& n)
+{
+    switch (n)
+    {
+    case 0: return 1.0;
+    case 1: return 1.0;
+    case 2: return 2.0;
+    case 3: return 6.0;
+    case 4: return 24.0;
+    case 5: return 120.0;
+    case 6: return 720.0;
+    case 7: return 5040.0;
+    case 8: return 40320.0;
+    case 9: return 362880.0;
+    case 10: return 3628800.0;
+    
+    default:
+        if(n < 0)
+        {
+            cout << "ERROR: factorial is called for a negative number!" << endl;
+            exit(99);
+        }
+        double tmp = 3628800.0;
+        for(int ii = 11; ii <= n; ii = ii + 1)
+        {
+            tmp = tmp * ii;
+        }
+        return tmp;
+    }
+}
+
 GTO::GTO()
 {
 }
@@ -30,7 +87,8 @@ void GTO::readBasis()
         }
         else
         {
-            
+            cout << "GOTO ERROR" << endl;
+            exit(999);
         }
 
         gtos.resize(size);
@@ -38,11 +96,8 @@ void GTO::readBasis()
         for(int ii = 0; ii < size; ii++)
         {
             ifs >> gtos(ii).a >> coeff(ii);
-            // cout << gtos(ii).a << endl;
-            // cout << coeff(ii) << endl;
-            gtos(ii).lx = 0;
-            gtos(ii).ly = 0;
-            gtos(ii).lz = 0;
+            gtos(ii).l = 0;
+            gtos(ii).m = 0;
         }
 
         
@@ -64,7 +119,32 @@ void GTO::normalization()
     return;
 }
 
+double GTO::auxiliary(const int& l, const double& a)
+{
+    int n = l / 2;
+    if(n*2 == l)    return double_factorial(2*n-1)/pow(a,n)/pow(2.0,n+1)*sqrt(PI/a);
+    else    return factorial(n) /2.0/pow(a,n+1);
+}
+
 double GTO::overlap(const gto_single& gto1, const gto_single& gto2)
 {
-    return sqrt(3.1415926535897932384626 / (gto1.a + gto2.a));
+    if(gto1.l != gto2.l || gto1.m != gto2.m) return 0.0;
+    else return auxiliary(2 + 2*gto1.l, gto1.a + gto2.a);
+}
+
+double GTO::nuc_attraction(const gto_single& gto1, const gto_single& gto2)
+{
+    if(gto1.l != gto2.l || gto1.m != gto2.m) return 0.0;
+    else return -atomN * auxiliary(1 + 2*gto1.l, gto1.a + gto2.a);
+}
+
+double GTO::kinetic(const gto_single& gto1, const gto_single& gto2)
+{
+    if(gto1.l != gto2.l || gto1.m != gto2.m) return 0.0;
+    else return gto2.a * (2*gto1.l + 3) * auxiliary(2 + 2*gto1.l, gto1.a + gto2.a) - 2 * gto2.a * gto2.a * auxiliary(4 + 2*gto1.l, gto1.a + gto2.a);
+}
+
+double GTO::h_1e(const gto_single& gto1, const gto_single& gto2)
+{
+    return kinetic(gto1, gto2) + nuc_attraction(gto1, gto2);
 }
--- gto_test/gto.h
+++ gto_test/gto.h
@@ -6,9 +6,11 @@
 using namespace std;
 using namespace Eigen;
 
+const double PI = asin(1.0) * 2.0;
+
 struct gto_single
 {
-    int lx, ly, lz;
+    int l, m;
     double a;
     Vector3d center;
 };
@@ -17,7 +19,7 @@ struct gto_single
 class GTO
 {
 private:
-    int size, angular;
+    int size, angular, atomN = 1;
     Matrix<gto_single, Dynamic, 1> gtos; 
     Vector3d center;
     VectorXd coeff;
@@ -29,7 +31,12 @@ public:
     void readBasis();
 
     void normalization();
+
+    double auxiliary(const int& l, const double& a);
     double overlap(const gto_single& gto1, const gto_single& gto2);
+    double nuc_attraction(const gto_single& gto1, const gto_single& gto2);
+    double kinetic(const gto_single& gto1, const gto_single& gto2);
+    double h_1e(const gto_single& gto1, const gto_single& gto2);
     
 };
 
